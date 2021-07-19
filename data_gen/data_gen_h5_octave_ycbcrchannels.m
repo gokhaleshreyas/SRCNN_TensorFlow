85fa264352230236950bdf6ff78d3a2c544df77c
Shreyas Gokhale <55978108+gokhaleshreyas@users.noreply.github.com>
2021-07-19 10:42:14 +0530

    Update data_gen_h5_octave_ycbcrchannels.m

--- data_gen/data_gen_h5_octave_ycbcrchannels.m
+++ data_gen/data_gen_h5_octave_ycbcrchannels.m
@@ -10,13 +10,14 @@ scale = 3;
 stride = 14;
 
 dataset='91';
-dataset_path = 'C:\Shreyas\Programming\Python\Neural_Networks\Data\Dataset\';
+dataset_path = '/Dataset/';
 data_path = strcat(dataset_path, dataset);
 
-save_data = 'train_91_ycbcrchannels_octave_v2.h5';
-save_dataset_path_1 = 'C:\Shreyas\Programming\Python\Neural_Networks\SRCNN\Data';
-save_dataset_path_2 = '\RGB2YCBCR_OCTAVE\';
-save_dataset_path = strcat(save_dataset_path_1, save_dataset_path_2);
+save_data = 'train_91_rgbchannels_octave.h5';
+% save_dataset_path_1 = '/train';
+% save_dataset_path_2 = '/MATLAB/';
+% save_dataset_path = strcat(save_dataset_path_1, save_dataset_path_2);
+save_dataset_path = '../train/';
 save_path = strcat(save_dataset_path, save_data);
 if ~exist(save_dataset_path)
     mkdir(save_dataset_path);
