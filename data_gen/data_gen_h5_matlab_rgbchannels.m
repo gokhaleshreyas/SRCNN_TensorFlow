33a037a73be54979a092812e91f74b8cf285425e
Shreyas Gokhale <55978108+gokhaleshreyas@users.noreply.github.com>
2021-07-19 10:35:36 +0530

    Update data_gen_h5_matlab_rgbchannels.m

--- data_gen/data_gen_h5_matlab_rgbchannels.m
+++ data_gen/data_gen_h5_matlab_rgbchannels.m
@@ -12,9 +12,10 @@ dataset_path = '/Dataset/';
 data_path = strcat(dataset_path, dataset);
 
 save_data = 'train_91_rgbchannels_matlab.h5';
-save_dataset_path_1 = '/train';
-save_dataset_path_2 = '/MATLAB/';
-save_dataset_path = strcat(save_dataset_path_1, save_dataset_path_2);
+% save_dataset_path_1 = '/train';
+% save_dataset_path_2 = '/MATLAB/';
+% save_dataset_path = strcat(save_dataset_path_1, save_dataset_path_2);
+save_dataset_path = '../train/'
 save_path = strcat(save_dataset_path, save_data);
 if ~exist(save_dataset_path)
     mkdir(save_dataset_path);
